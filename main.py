bb6939d06c072f813851aa9910141208db9deccb
jonnycanabal <jonny.canabal@gmail.com>
2022-09-12 21:50:20 -0500

    Declaración, Print, Input en Python

---
+++ main.py
@@ -0,0 +1,24 @@
+#Ejemplos de Variables
+"""
+mensaje = "Hola mundo esoty aprendiendo Python"
+print(mensaje)
+"""
+nombre = "Jonny"
+apellido = "Canabal"
+ciudad = "Bogota D.C."
+celular = 3183865656
+
+print(nombre)
+print(nombre+" "+apellido+" "+ciudad+" "+str(celular))
+print(nombre,apellido,ciudad,celular)
+
+print(f"hola {nombre} {apellido} de la ciudad {ciudad} con celular {celular}")
+
+print("hola %s de %s"%(nombre,ciudad))
+
+#Leer datos desde teclado
+nombre = input("Digite tu nombre: ")
+print(nombre)
+
+
+
