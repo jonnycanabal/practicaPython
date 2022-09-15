db467547bc8eb2ef03c556b6285934b73db4b73d
jonnycanabal <jonny.canabal@gmail.com>
2022-09-14 20:40:26 -0500

    Se agrega nuevo archivo llamado ""ciclos"

---
+++ ciclos.py
@@ -0,0 +1,25 @@
+# Ciclo iterativo while
+"""
+manzanas = 5
+contador = 0
+
+print (f"Se ha iniciado el carrito. En total hay {contador} manzanas")
+
+while(contador < manzanas):
+    contador = contador+1
+    print(f"Se ha agregado una manzana a la canasta. Ahora hay {contador} manzanas.")
+"""
+
+# Ciclo iterativo for controlado por cantidad
+
+print ("Se ha iniciado el carrito. En total hay 0 manzanas")
+for i in range(1,6):
+    print(f"Se ha agregado una manzana a la canasta. Ahora hay {i} manzanas")
+
+
+
+
+
+
+
+
